8cb0fb66468fa8238531377cd8769d25e657dd4f
drb1220 <david.1220.bunger@gmail.com>
2025-09-10 12:03:40 -0400

    hw01 start

---
+++ hw01.py
@@ -0,0 +1,7 @@
+
+# input: Two natural numbers B and C — the results of Bob's and Charlie's die rolls.
+def answer(B,C):
+    # output: A string with the probability of Alice winning in the form of an 
+    # irreducible fraction in format A/B, where A — the numerator, and B — the 
+    # denominator.
+    return "A/B"
