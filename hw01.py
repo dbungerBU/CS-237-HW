f72624325b2f252dd4d134c30e5a573da8306bf8
drb1220 <david.1220.bunger@gmail.com>
2025-09-10 12:32:37 -0400

    hw01 finished

--- hw01.py
+++ hw01.py
@@ -4,4 +4,14 @@ def answer(B,C):
     # output: A string with the probability of Alice winning in the form of an 
     # irreducible fraction in format A/B, where A — the numerator, and B — the 
     # denominator.
-    return "A/B"
+    high = max(B,C)
+    num = 7-high
+    denom = 6
+    if(num % 2 == 0):
+        num = num/2
+        denom = denom/2
+    if(num % 3 == 0):
+        num = num/3
+        denom = denom/3
+    return str(int(num))+"/"+str(int(denom))
+print(answer(6,1))
