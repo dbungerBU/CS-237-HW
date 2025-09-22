9dcbfab8e7e17e139d8ca2e23a39f6fd7faeccce
drb1220 <david.1220.bunger@gmail.com>
2025-09-22 17:03:08 -0400

    HW 3 started

---
+++ hw03.py
@@ -0,0 +1,14 @@
+# INPUT:
+# Eight integer numbers, 0 <= X1, Y1, X2, T2, X3, Y3, X4, Y4 <= 10, such that:
+# (X1, Y1) is the bottom-left coordinate of the first rectangle;
+# (X2, Y2) is the top-right coordinate of the  first rectangle;
+# (X3, Y3) is the bottom-left coordinate of the second rectangle;
+# (X4, Y4) is the top-right coordinate of the second rectangle;
+
+# OUTPUT
+# The probability of hitting the intersection area if a dart is thrown randomly within the 10x10 region.
+# Your answer should be expressed as float number within the interval [0.0,1.0]
+def answer(x1,y1,x2,y2,x3,y3,x4,y4):
+    probability = 0.0
+    return probability
+
