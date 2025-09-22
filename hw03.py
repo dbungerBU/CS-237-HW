e1fd6982a530eb18581523562a2717bd865bb645
drb1220 <david.1220.bunger@gmail.com>
2025-09-22 17:32:32 -0400

    HW 3 complete

--- hw03.py
+++ hw03.py
@@ -9,6 +9,12 @@
 # The probability of hitting the intersection area if a dart is thrown randomly within the 10x10 region.
 # Your answer should be expressed as float number within the interval [0.0,1.0]
 def answer(x1,y1,x2,y2,x3,y3,x4,y4):
-    probability = 0.0
+    if not((x1<=x3<=x2 or x3<=x1<=x4) and (y1<=y3<=y2 or y3<=y1<=y4)):
+        return 0.0
+    xsorted = sorted([x1,x2,x3,x4])
+    ysorted = sorted([y1,y2,y3,y4])
+    w = xsorted[2]-xsorted[1]
+    h = ysorted[2]-ysorted[1]
+    probability = (w*h)/100
     return probability
 
